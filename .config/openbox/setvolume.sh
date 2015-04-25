ec6d60b5b0921ac77ebc814093963a3424fc29e1
Yamagi Burmeister <yamagi@yamagi.org>
2015-04-25 14:00:40 +0200

    Some changes to the Openbox configuration:
    - Integrate XF86 function keys (mainly for laptops)
    - Mute audio on Linux
    - Do not hardcode monitors
    - Reformat the rc.xml

--- .config/openbox/setvolume.sh
+++ .config/openbox/setvolume.sh
@@ -3,7 +3,7 @@
 mode=$1
 
 if [ $mode = "louder" ]; then
-	case $OSTYPE in
+	case "$(uname -s)" in
 		FreeBSD)
 			/usr/sbin/mixer vol +1 > /dev/null
 			;;
@@ -14,7 +14,7 @@ if [ $mode = "louder" ]; then
 
 	esac
 elif [ $mode = "quieter" ]; then
- 	case $OSTYPE in
+	case "$(uname -s)" in
 		FreeBSD)
 			/usr/sbin/mixer vol -1 > /dev/null
 			;;
@@ -24,4 +24,11 @@ elif [ $mode = "quieter" ]; then
 			;;
 
 	esac 
+elif [ $mode = "mute" ]; then
+	case "$(uname -s)" in
+		linux)
+			amixer sset Master toggle > /dev/null
+			;;
+
+	esac
 fi
