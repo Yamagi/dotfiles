bcb90978023cc65acd2ae4810ce0e9ba60b07634
Yamagi Burmeister <yamagi@yamagi.org>
2017-11-06 17:32:25 +0100

    Move the vi mode indicator to the left prompt.

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -1,8 +1,23 @@
 # Prompt
 # ------
 
-# Left prompt (user@host:pty path:)
-PS1='%n@%m:%l %(4~|.../%3~|%~)%(!|>|:) '
+# Left prompt. This deserves some explantion: We want
+# a small indicator for the current vi mode. '+' is
+# insert mode and '-' is command mode. The shell starts
+# in insert mode, so the standard prompt is '[%*]+'.
+# Whenever the mode is changed or a new line is began
+# zsh calls 'zle-line-init' or 'zle-keymap-select'. So
+# we register both function, evaluate the current state
+# of the KEYMAP variable to determine the mode.
+PS1='%n@%m:%l + %(4~|.../%3~|%~)%(!|>|:) '
+
+function zle-line-init zle-keymap-select {
+	PS1="%n@%m:%l ${${KEYMAP/vicmd/-}/(main|viins)/+} %(4~|.../%3~|%~)%(!|>|:) "
+	zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
 
 # Second level prompt
 PS2='%_> '
@@ -13,21 +28,5 @@ PS3='?# '
 # Forth level prompt
 PS4='+ '
 
-# Right prompt. This deserves some explantion: We want
-# a small indicator for the current vi mode. '+' is
-# insert mode and '-' is command mode. The shell starts
-# in insert mode, so the standard prompt is '[%*]+'.
-# Whenever the mode is changed or a new line is began
-# zsh calls 'zle-line-init' or 'zle-keymap-select'. So
-# we register both function, evaluate the current state
-# of the KEYMAP variable to determine the mode.
-RPS1="[%*]+"
- 
-function zle-line-init zle-keymap-select {
-	RPS1="[%*]${${KEYMAP/vicmd/-}/(main|viins)/+}"
-	zle reset-prompt
-}
-
-zle -N zle-line-init
-zle -N zle-keymap-select
-
+# Right prompt
+RPS1="[%*]"
