0930925d46110d3149bfe368d69380d3f08befd7
Yamagi Burmeister <yamagi@yamagi.org>
2017-11-06 17:22:33 +0100

    Some corrections to the new bindings.
    * Bind k and j in vi normal mode.
    * Bind all possible cursor key sequences. At least the terminfo on my
      desktop is incorrect and we need some work arounds... :/

--- .zsh/config/bindings.zsh
+++ .zsh/config/bindings.zsh
@@ -4,6 +4,11 @@
 # vi mode, of course :)
 bindkey -v
 
+# The standard key timeout is 0.4 seconds
+# which is really annoying. Reduce it to
+# 0.1 seconds.
+export KEYTIMEOUT=1
+
 # Widgets for tcsh like history behavior
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
@@ -14,9 +19,17 @@ zle -N history-beginning-search-forward-end history-search-end
 bindkey '^i' expand-or-complete-prefix
 
 # Search history up and down on
-# the "up" and "down" arrow keys.
+# the "up" and "down" arrow keys
+# and on "k" and "j" in vi normal
+# mode. Also bind the raw key
+# sequences to work around broken
+# terminfo entries.
 bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey -a "k" history-beginning-search-backward-end
 bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
+bindkey "^[[B" history-beginning-search-forward-end
+bindkey -a "j" history-beginning-search-forward-end
 
 # Delete on 'del'
 bindkey "${terminfo[kdch1]}" vi-delete-char
