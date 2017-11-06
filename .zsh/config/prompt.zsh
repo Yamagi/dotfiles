8ec6f57a630b70d9598182b7783d92f1617002bb
Yamagi Burmeister <yamagi@yamagi.org>
2017-11-06 11:05:45 +0100

    Indicate the vi mode at the right hand side.
    + is insert mode and - is command mode. maybe I'll change this again
    when I find a more aesthetic way to indicate the mode.

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -13,6 +13,21 @@ PS3='?# '
 # Forth level prompt
 PS4='+ '
 
-# Right prompt
-RPS1='[%*]'
+# Right prompt. This deserves some explantion: We want
+# a small indicator for the current vi mode. '+' is
+# insert mode and '-' is command mode. The shell starts
+# in insert mode, so the standard prompt is '[%*]+'.
+# Whenever the mode is changed or a new line is began
+# zsh calls 'zle-line-init' or 'zle-keymap-select'. So
+# we register both function, evaluate the current state
+# of the KEYMAP variable to determine the mode.
+RPS1="[%*]+"
  
+function zle-line-init zle-keymap-select {
+	RPS1="[%*]${${KEYMAP/vicmd/-}/(main|viins)/+}"
+	zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
