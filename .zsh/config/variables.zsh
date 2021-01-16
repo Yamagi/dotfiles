ccb19d6e3603beac97c8bbafbc6537e88c157664
Yamagi <yamagi@yamagi.org>
2021-01-16 09:22:08 +0100

    Always set the locale through the shell.
    This removes the FreeBSD special case were the locale can be set through
    login(1). It saves one files and makes the shell config a little bit
    more straight forward.

--- .zsh/config/variables.zsh
+++ .zsh/config/variables.zsh
@@ -1,11 +1,8 @@
 # Variables
 # ---------
 
-# On Linux the LANG variable must be set at
-# shell level. login(8) is unable to do it.
-if [[ $(uname -s) == "Linux" ]] ; then
-	export LANG=de_DE.UTF-8
-fi
+# German locale.
+export LANG=de_DE.UTF-8
 
 # 'vim' as default editor
 export EDITOR=vim
