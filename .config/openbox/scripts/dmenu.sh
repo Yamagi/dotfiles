a33ec5faa735dbac9b13b548e05616e447a714ba
Yamagi <yamagi@yamagi.org>
2021-04-09 16:45:03 +0200

    Remove everything that's not needed by the Gnome desktop.
    These are the configs for the X11 stack, including openbox and tint2.
    The Terminus font, because the time for bitmap fonts is over. And all
    assets like icons and themes.

--- .config/openbox/scripts/dmenu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Wrapper script that starts / runs dmenu
-# with the appropriate options.
-
-. ~/.config/openbox/scripts/openbox.subr
-
-runcmd dmenu_run -p ">" -m 0 -b -nb \#4c555d -nf \#ffffff -sb \#4c555d -sf \#ffffff -fn "Fira Sans-9"
