ad786a7ca3fe93537f714afb0f53339b57bcbe1c
Yamagi <yamagi@yamagi.org>
2022-10-11 14:19:29 +0200

    Rework the tmux config once again.
    * Add magic to renumber sessions if one gets closed.
    * Reorder the options by alphabet.

---
+++ .config/tmux/scripts/renumber-sessions.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Renumbers tmux sessions if one was closed.
+
+sessions=$(tmux list-sessions -F '#S' | grep '^[0-9]\+$' | sort -n)
+new=1
+
+for old in $sessions ; do
+	if [ $old -ne $new ] ; then
+		tmux rename -t $old $new
+	fi
+
+	new=$(($new + 1))
+done
